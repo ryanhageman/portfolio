df5063f7b193bfc6a6a6b72e62581ae45e9093ce
Ryan Hageman <ryanhageman@icloud.com>
2024-07-27 13:44:51 -0500

    refactor: fix linting issues

--- config.rb
+++ config.rb
@@ -9,21 +9,21 @@ end
 # https://middlemanapp.com/basics/layouts/
 
 # Per-page layout changes
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
+page "/*.xml", layout: false
+page "/*.json", layout: false
+page "/*.txt", layout: false
 
 # With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
+# page "/path/to/file.html", layout: "other_layout"
 
 # Proxy pages
 # https://middlemanapp.com/advanced/dynamic-pages/
 
 # proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
+#   "/this-page-has-no-template.html",
+#   "/template-file.html",
 #   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
+#     which_fake_page: "Rendering a fake page with a local variable"
 #   },
 # )
 
@@ -33,7 +33,7 @@ page '/*.txt', layout: false
 
 # helpers do
 #   def some_helper
-#     'Helping'
+#     "Helping"
 #   end
 # end
 
