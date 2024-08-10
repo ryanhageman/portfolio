24edc896f374d52956cc4a3b94f3ea7dd114bd87
Ryan Hageman <ryanhageman@icloud.com>
2024-08-10 15:44:41 -0500

    chore: clean up config
    Delete boilerplate comments.
    Group and organize with comment lines.

--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 require "slim"
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+
+# ────────────────────────────── Extensions ──────────────────────────────
 
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
@@ -8,40 +8,14 @@ end
 
 activate :livereload
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
+# ─────────────────────────────── Layouts ─────────────────────────────
 
 # Per-page layout changes
 page "/*.xml", layout: false
 page "/*.json", layout: false
 page "/*.txt", layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: "other_layout"
-
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   "/this-page-has-no-template.html",
-#   "/template-file.html",
-#   locals: {
-#     which_fake_page: "Rendering a fake page with a local variable"
-#   },
-# )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
+# ──────────────────────────────── Build ──────────────────────────────
 
 # configure :build do
 #   activate :minify_css
