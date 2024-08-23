3a703ab34aa3f1697ab4115cb3f70fc02492453a
Ryan Hageman <ryanhageman@icloud.com>
2024-08-23 17:44:18 -0500

    fix: update config to prefix the repo name to the links

--- config.rb
+++ config.rb
@@ -19,6 +19,7 @@ page "/*.txt", layout: false
 
 configure :build do
   set :build_dir, "docs" # GitHub Pages publishes this folder
+  set :http_prefix, "/portfolio"
 
   # Compile for publication
   activate :relative_assets
