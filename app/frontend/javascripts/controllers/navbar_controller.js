58430c06e789ce4c5987841bf58a9a98fd4a840f
Ryan Hageman <ryanhageman@icloud.com>
2024-09-05 18:53:51 -0500

    feat: setup navbar_controller
    ## Navbar Controller
    
    Setup the Stimulus Navbar Controller to handle showing and hiding the
    mobile navigation menu.

---
+++ app/frontend/javascripts/controllers/navbar_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  static targets = ['hamburger', 'menu']
+
+  toggleMenu() {
+    this.hamburgerTarget.classList.toggle('is-open')
+    this.menuTarget.classList.toggle('is-open')
+  }
+}
