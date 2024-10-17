627d6af5cfd2fb542974cb8f9c17fb57092fee1d
Ryan Hageman <ryanhageman@icloud.com>
2024-10-16 19:19:32 -0500

    feat: default fold levels

--- app/frontend/javascripts/controllers/resume_fold_controller.js
+++ app/frontend/javascripts/controllers/resume_fold_controller.js
@@ -2,8 +2,13 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   static targets = ['foldable', 'foldToggle']
+  static values = { folded: String }
 
-  toggleFold () {
+  connect (  ) {
+    if (this.foldedValue === "true") this.toggleFold()
+  }
+
+  toggleFold() {
     this.foldToggleTarget.classList.toggle('is-closed')
     this.foldableTarget.classList.toggle('is-closed')
   }
