6e8bc9889d0a4126bea3862d844cadd00319e2ca
Ryan Hageman <ryanhageman@icloud.com>
2024-10-27 09:42:10 -0500

    feat: extract frameworks from language in data file
    ## Extract Frameworks
    
    Frameworks are sub lists for languages. This pulls them out of the name
    string and gives them their own entries. It also adds tags to the data
    for the upcoming filter functionality. Now frameworks can have their own
    individual tag and there is more flexibility in how we render them.

---
+++ helpers/resume_helpers.rb
@@ -0,0 +1,31 @@
+module ResumeHelpers
+  def tech_summary_group_list(items)
+    items.map { |item| tech_summary_entry(item) }.join(", ")
+  end
+
+  def tech_summary_element(item)
+    content_tag(:span,
+      item.name,
+      data: {action: "click->resume-filter#show",
+             resume_filter_tag_value: item.tag})
+  end
+
+  private
+
+  def tech_summary_entry(item)
+    entry = []
+    entry << tech_summary_element(item)
+    entry << tech_summary_frameworks(item)
+    entry.join
+  end
+
+  def tech_summary_frameworks(language)
+    return if language.frameworks.blank?
+
+    items = language.frameworks.map do |framework|
+      tech_summary_element(framework)
+    end
+
+    " (#{items.join(", ")})"
+  end
+end
